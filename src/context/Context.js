d2e839a26c1346c5a39a33d25560f43402a43473
Lenovo <anand9412868527@gmail.com>
2022-01-20 21:44:12 +0530

    some changes

--- src/context/Context.js
+++ src/context/Context.js
@@ -4,6 +4,7 @@ import { CartReducer, productReducer } from "./Reducers";
 
 const Cart = createContext();
 faker.seed(99);
+
 const Context = ({ children }) => {
   const products = [...Array(20)].map(() => ({
     id: faker.datatype.uuid(),
